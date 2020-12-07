807f7a7828d5a1a8ce1285fe5a1535971fb63d80
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-12-07 13:24:29 +0530

    commit setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+virtualenv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+git clone --recurse-submodules https://github.com/mlperf/inference.git mlperf_inference
+
+cd mlperf_inference/loadgen
+
+CFLAGS="-std=c++14 -O3" python setup.py bdist_wheel
+
+for f in dist/*; do
+    pip install --force-reinstall $f
+    break
+done
+
+echo "Setup done"
